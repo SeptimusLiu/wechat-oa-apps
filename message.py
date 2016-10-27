8e5ee6efabb84a4a52f622296ba02db081cd9f12
danydliu <danydliu@tencent.com>
2016-10-27 18:39:55 +0800

    first commit

---
+++ message.py
@@ -0,0 +1,13 @@
+import xml.etree.ElementTree as ET
+
+class MsgFactory(object):
+    def __init__(self, xml_str):
+        if not xml_str:
+            return None
+        xml_data = ET.fromstring(xml_str)
+        self.to_user = ET.find('ToUserName').text
+        self.from_user = ET.find('FromUserName').text
+        self.content = ET.find('Content').text
+        self.msg_id = ET.find('MsgId').text
+        self.msg_type = ET.find('MsgType').text
+        # if (msg_type == '')
